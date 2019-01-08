20d8188aa8052990ed8d929e3cdc7170b7d5bfea
salinger760 <salinger760@gmail.com>
2019-01-08 11:45:44 +0900

    Modify login

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,13 @@
 from flask import Flask, Response, request
 from flask_cors import CORS
+import os, sys, json, logging
+import pymysql.cursors
+
+logging.getLogger('flask_cors').level = logging.DEBUG
 
 app = Flask(__name__)
 CORS(app, resources={"/auth*": {"origins": "http://localhost:8080"}})
+
 
 @app.route('/')
 def pong():
@@ -11,4 +16,30 @@ def pong():
 
 @app.route('/auth')
 def auth():
-  return 'hogehoge'
+  flg = False
+  uname = request.args.get('username', '')
+  upass = request.args.get('password', '')
+
+
+  conn = pymysql.connect(host='127.0.0.1',
+                    user='root',
+                    password='password',
+                    db='testdb',
+                    charset='utf8mb4',
+                    cursorclass=pymysql.cursors.DictCursor)
+
+
+  try:
+    with conn.cursor() as cursor:
+      sql = "SELECT user_id, name FROM users WHERE user_id = %s"
+      cursor.execute(sql, ('1',))
+      rows = cursor.fetchall()
+      print(rows)
+  finally:
+    conn.close()
+
+  for row in rows:
+    if upass == row[0]:
+      return json.dumps({"result": "success"})
+
+  return json.dumps({"result": "fail"})
--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -19,6 +19,10 @@
 </template>
 
 <script>
+import Axios from 'axios'
+import jwt from 'jsonwebtoken'
+import router from '../router/index.js'
+
 export default {
   name: 'login',
   data: function(){
@@ -29,24 +33,73 @@ export default {
   },
   methods: {
     checklogin: function(event){
-      let nextPage = this.$route.query.nextPage
-      if(nextPage === undefined){
+      const axios = Axiot.create({
+        baseURL: 'http://localhost:4000',
+        header: {
+          'Content-Type': 'application/json',
+          'X-Requested-With': 'XMLHttpRequest'
+        },
+        responseType: 'json'
+      })
+
+      // nextページの取得
+      try{
+        let nextPage = this.$route.query.next
+      }catch(e){
         nextPage = 'PageA'
       }
-      
       console.log(nextPage)
 
-      const correntLoginName = 'myLoginName'
-      const correntPassword = 'mySecretPassword1234'
+      // axiosのthenメソッドの中ではthisがvueコンポーネントを指さなくなるので別の変数に割り当てておく
+      const self = this
 
-      let myLoginName = document.getElementById('loginaccount').value
-      let myLoginPass = document.getElementById('loginpassword').value
+      axios.get('/auth', {
+        // クエリパラメータをセット
+        params: {
+          username: document.getElementById('loginaccount').value,
+          password: document.getElementById('loginpassword').value
+        }
+      }).then(function(response) {
+        // 応答が戻ってきたら結果を処理。
+        // "success" だったら次のページにジャンプし、失敗だったらその旨表示
+        if(response.data.result === 'success'){
+          let token = self.generateToken(document.getElementById('loginaccount').value)
+          // cookieとしてトークンを付与
+          document.cookie = 'token=' + token
+          // 次のページにジャンプ
+          // router.push({name: nextPage, params: { auth: 'authenticated' }})
+          router.push({name: nextPage})
+        } else {
+          document.getElementById('loginResult').innerHTML = 'Login Failed !'
+        }
+      })
+    },
+    generateToken: function(username){
+      /* JWTトークンの生成 */
+      // JWT用のシークレットトークンをセット(【注意】実際にはコードの中に書いてはいけない！)
+      const secretToken = 'oreore'
 
-      if( (myLoginName === correntLoginName) && (myLoginPass === correntPassword) ){
-        this.$router.push({name: nextPage, query: {auth: 'authenticated' }})
-      }else{
-        document.getElementById('loginResult').innerHTML = 'Login Failed !'
-      }
+      // JWTのヘッダー
+      const oHeader = {alg: 'HS256', typ: 'JWT'}
+
+      // JWTペイロード
+      let offset = Math.floor(Math.random() * Math.floor(25))
+      const DO = new Date()
+      DO.setSeconds(DO.getSeconds() + offset)
+      let dY = DO.getFullYear()
+      let dm = DO.getMonth() + 1
+      let dd = DO.getDate()
+      let dH = ('0' + DO.getHours()).slice(-2)
+      var dM = ('0' + DO.getMinutes()).slice(-2)
+      var dS = ('0' + DO.getSeconds()).slice(-2)
+      var dStr = dY + '/' + dm + '/' + dd + ' ' + dH + ':' + dM + ':' + dS + ' +0900'
+      var oPayload = {username: username, until: dStr}
+      console.log(oHeader, oPayload)
+      // JWTを生成
+      var sJWT = jwt.sign({ foo: 'bar' }, secretToken, { algorithm: 'HS256'});
+      // var sJWT = KJUR.jws.JWS.sign('HS256', JSON.stringify(oHeader), JSON.stringify(oPayload), secretToken)
+      console.log(sJWT)
+      return sJWT
     }
   }
 }
